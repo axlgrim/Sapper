1337dfa62615f4fbcaffdd8b8da5968037bf510c
Axl <alexshasholko@gmail.com>
2018-06-19 23:57:59 +0300

    Commit of Sapper project

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    public SpriteRenderer SpriteRend;
+    public Sprite Face;
+    public Sprite Back;
+    public Sprite Mine;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SpriteRend.sprite = Back;
+    }
+
+    void OnMouseUp()
+    {
+        ChangeSprite();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void ChangeSprite()
+    {
+        SpriteRend.sprite = Face;
+            
+    }
+}
